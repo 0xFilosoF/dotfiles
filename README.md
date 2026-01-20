aac848ecfa11cb26735312a7545fd839ce31e8ca
0xFilosoF <roman.lashnev@mail.ru>
2026-01-20 23:07:31 +0300

    style: use stow

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # dotfiles
 
-### WIP Choosing between chezmoi VS stow
+### Installation
+
+[Stow](https://www.gnu.org/software/stow/) is a tool for managing symlinks to files and directories.
+
+### Setup with stow:
+```bash
+stow .
+```
---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# ----------------------------------------------------------
+# here can be the installation of your favorite packages
+# but it was decided to make the installation more modular
+# ----------------------------------------------------------
+
+stow .
